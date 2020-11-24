de0ca08b199cd127c30eeddad80decf3152db087
Vladimir Romashov <v.romashov@tue.nl>
2020-11-24 11:37:48 +0100

    US 55 rebased on Master removed files not related to this US - Adding Logger class for logging - using Configuration manager API to load yaml config file

---
+++ src/output/logger.py
@@ -0,0 +1,92 @@
+"""
+Date: 19 November 2020
+Authors: Hossein Mahdian, Vladimir Romashov
+
+This is an output module script that is able to print emotion destribution from
+raspberry camera stream or input video.
+
+The script is able to print output in a file or in command line.
+"""
+
+import logging as log
+import os, sys
+current_directory = os.getcwd()
+parent_directory = os.path.dirname(current_directory)
+grand_parent_directory = os.path.dirname(parent_directory)
+sys.path.insert(0, grand_parent_directory)
+from src.configuration_manager import ConfigurationManager
+
+
+class Logger:
+    """
+    This class manages logs in console, file, or both
+    """
+    
+    __app_log = None
+    __stream_handler = None
+    __file_handler = None
+
+    def __init__(self):
+        configuration_manager = ConfigurationManager("configuration.yml")
+        config = configuration_manager.configuration
+        self.__prediction_conf = config['video']['prediction']
+        self.__init_logger()
+
+    def __init_logger(self):
+
+        # Get our logger
+        self.__app_log = log.getLogger('root')
+        self.__app_log.setLevel(log.INFO)
+        log_formatter = log.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s',
+                                       datefmt='%d/%m/%Y %H:%M:%S')
+
+        # Setup Stream Handler (i.e. console)
+        self.__stream_handler = log.StreamHandler()
+        self.__stream_handler.setFormatter(log_formatter)
+        self.__stream_handler.setLevel(log.INFO)
+        # File to log to
+        if self.__prediction_conf['output_type'] != 'CMD':
+            log_file = self.__prediction_conf['log_file_path'] + "log.log"
+            # Setup File handler
+            self.__file_handler = log.FileHandler(log_file)
+            self.__file_handler.setFormatter(log_formatter)
+            self.__file_handler.setLevel(log.INFO)
+
+    def logs(self, result):
+        """
+        gets the result of a prediction and log it based on config file
+        :param output_type:
+        :param result:
+        """
+        if self.__prediction_conf['output_type'] == 'CMD':
+            self.__logs_cmd(result)
+        elif self.__prediction_conf['output_type'] == 'File':
+            self.__logs_file(result)
+        else:
+            self.__logs_cmd(result)
+            self.__logs_file(result)
+
+    def __logs_file(self, result):
+        max_result = result.max()
+        max_position = result.argmax()
+        self.__app_log.addHandler(self.__file_handler)
+        self.__app_log.info("Emotion distribution set: {}".format(str(result)))
+        self.__app_log.info(
+            "Detected emotion category : {} probability: {}".format(str(max_position), str(max_result)))
+        self.__app_log.info("")
+
+    def __logs_cmd(self, result):
+        max_result = result.max()
+        max_position = result.argmax()
+        # Add the Handler
+        self.__app_log.addHandler(self.__stream_handler)
+        self.__app_log.info("Emotion distribution set: {}".format(str(result)))
+        self.__app_log.info(
+            "Detected emotion category : {} probability: {}".format(str(max_position), str(max_result)))
+
+    def info(self, message):
+        """
+        gets a Message and add it to log
+        :param message:
+        """
+        self.__app_log.info(message)
