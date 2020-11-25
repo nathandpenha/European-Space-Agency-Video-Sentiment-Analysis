548afbd6cc12a3caf06fbebcd92063dcdfee8cfd
Hossein Mahdian <hossein.mahdiyan@yahoo.com>
2020-11-25 16:50:28 +0100

    - running optimized model on laptop video - running h5 model on laptop video - running h5 model on laptop camera

--- src/inference/prediction.py
+++ src/inference/prediction.py
@@ -33,7 +33,6 @@ class Prediction:
     __model_blob = None
     __model_type = None
     __output_type = None
-    __mode = None  # RPi, Webcam (for demo), Record, Image (optional)
     __logger = None
 
     def __init__(self):
@@ -53,55 +52,6 @@ class Prediction:
         self.__face_detector = FaceDetector()
         self.__normalizer = Normalization(False, self.__prediction_conf['model_input_shape'][1]['height'])
 
-    def display_result(self, result, image):
-        """
-        This function shows histogram for the rPI camera input and/or provide a command line/text file representation
-        of the output data
-        :param result: result from prediction
-        :param image: captured image
-        """
-        if self.__prediction_conf['input_type'] == 'Camera':
-            for key, probes in result.items():
-                ir_result = probes[0]
-                self.__gui_output.draw_histogram(ir_result, image)
-                self.__logger.logs(ir_result)
-        elif self.__prediction_conf['input_type'] == 'Video':
-            self.__logger.logs(result)
-
-    def __is_video_input(self):
-        """
-        This function checks if Inference module is running for prerecorded video
-        or Camera
-        @:return True if video file was provided
-        """
-        if self.__prediction_conf['input_type'] == 'Camera':
-            return False
-        elif self.__prediction_conf['input_type'] == 'Video':
-            return True
-        raise Exception("input_type should be set to Camera or Video in Configuration")
-
-    def get_data(self, file):
-        """
-        This function get camera frame or video and returns normalized frames with detected faces
-        :param file: file can be a frame or video
-        :return:  frame or frames with detected face
-        """
-        if self.__prediction_conf['input_type'] == 'Camera' and self.__prediction_conf['model_format'] == 'IR':
-            image = file
-            face_image = self.__face_detector.get_frame(image)
-            if face_image is not None:
-                normalized_face = self.__normalizer.get_frame(face_image)
-                return normalized_face
-        # path to the folder with video
-        elif self.__prediction_conf['input_type'] == 'Video':
-            video = file
-            frames = self.__frame_generator.get_frames(video)
-            faces = self.__face_detector.get_frames(frames)
-            normalized_faces = self.__normalizer.get_frames(faces)
-            return normalized_faces
-        else:
-            raise Exception("check input type and model format in configuration!")
-
     def load_model(self):
         """
         This function:
@@ -114,79 +64,6 @@ class Prediction:
             self.model, self.model_blob = self.__get_optimized_model()
         else:
             raise ValueError("model format is not correct")
-
-    def predict_emotion(self):
-        """
-        This Function predicts the emotion based on its input type
-        """
-        if self.__is_video_input():
-            self.__predict_emotion_video()
-        else:
-            if self.__prediction_conf['is_rpi_camera']:
-                self.__predict_emotion_rpi_camera()
-            else:
-                self.__predict_emotion_webcam()
-
-    def __predict_emotion_video(self):
-        video = cv2.VideoCapture(self.__prediction_conf['input_directory'])
-        faces = self.get_data(video)
-        for face in faces:
-            self.__h5_model_execution(face)
-
-    '''def __predict_for_specific_model_format(self, face_image):
-        if self.__prediction_conf['model_format'] == 'h5':
-            self.__h5_model_execution(face_image)
-        elif self.__prediction_conf['model_format'] == 'IR':
-            self.__optimized_model_execution(face_image, self.model, self.model_blob)
-        else:
-            raise ValueError("model format is not correct")'''
-
-    def __h5_model_execution(self, face):
-        normalized_face_image = self.__image_reshape(face)
-        result = self.model.predict(normalized_face_image)
-        self.display_result(result, face)
-
-    def __image_reshape(self, image):
-        return image.reshape(1, self.__prediction_conf['model_input_shape'][1]['height'],
-                             self.__prediction_conf['model_input_shape'][2]['width'],
-                             self.__prediction_conf['model_input_shape'][0]['channels'])
-
-    def __predict_emotion_rpi_camera(self):
-        # initialize the camera and grab a reference to the raw camera capture
-        camera, rawCapture = self.__get_rPI_cam_input()
-        # capture frames from the camera
-        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-            # grab the raw NumPy array representing the image, then initialize the timestamp
-            # and occupied/unoccupied text
-            image = frame.array
-            self.__optimized_model_execution(image)
-            rawCapture.truncate(0)
-            key = cv2.waitKey(1) & 0xFF
-            # clear the stream in preparation for the next frame
-            rawCapture.truncate(0)
-            # if the `q` key was pressed, break from the loop
-            if key == ord("q"):
-                break
-
-    def __optimized_model_execution(self, image):
-        normalized_face = self.get_data(image)
-        if normalized_face is not None:
-            prepared_face_image = self.__prepare_image(normalized_face,
-                                                       self.model,
-                                                       self.model_blob)
-            ir_result = self.model.infer(inputs={self.model_blob: prepared_face_image})
-            self.display_result(ir_result, image)
-        cv2.imshow("Frame", image)
-
-    def __get_rPI_cam_input(self):
-        from picamera.array import PiRGBArray
-        from picamera import PiCamera
-        # initialize the camera and grab a reference to the raw camera capture
-        camera = PiCamera()
-        camera.resolution = (640, 480)
-        camera.framerate = 32
-        rawCapture = PiRGBArray(camera, size=(640, 480))
-        return camera, rawCapture
 
     def __get_optimized_model(self):
         from openvino.inference_engine import IECore
@@ -217,6 +94,148 @@ class Prediction:
         exec_net = ie.load_network(network=net, device_name=ir_run_conf['device'])
         return exec_net, input_blob
 
+    def predict_emotion(self):
+        """
+        This Function predicts the emotion based on its input type
+        """
+        if self.__is_video_input():
+            self.__predict_emotion_video()
+        else:
+            if self.__prediction_conf['is_rpi_camera']:
+                self.__predict_emotion_rpi_camera()
+            else:
+                self.__predict_emotion_webcam()
+
+    def __is_video_input(self):
+        """
+        This function checks if Inference module is running for prerecorded video
+        or Camera
+        @:return True if video file was provided
+        """
+        if self.__prediction_conf['input_type'] == 'Camera':
+            return False
+        elif self.__prediction_conf['input_type'] == 'Video':
+            return True
+        raise Exception("input_type should be set to Camera or Video in Configuration")
+
+    def __predict_emotion_video(self):
+        video = cv2.VideoCapture(self.__prediction_conf['input_directory'])
+        faces = self.get_data_from_video_file(video)
+        for face in faces:
+            self.__predict(face)
+
+    def get_data_from_video_file(self, file):
+        """
+        This function gets file and returns frames with detected face
+        :param file
+        :return: frames with detected face
+        """
+        if self.__is_video_input():
+            video = file
+            frames = self.__frame_generator.get_frames(video)
+            faces = self.__face_detector.get_frames(frames)
+            normalized_faces = self.__normalizer.get_frames(faces)
+            return normalized_faces
+        else:
+            raise Exception("check input type and model format in configuration")
+
+    def __predict_emotion_rpi_camera(self):
+        # initialize the camera and grab a reference to the raw camera capture
+        camera, rawCapture = self.__get_rPI_cam_input()
+        # capture frames from the camera
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            # grab the raw NumPy array representing the image, then initialize the timestamp
+            # and occupied/unoccupied text
+            image = frame.array
+            self.__predict(image)
+            rawCapture.truncate(0)
+            key = cv2.waitKey(1) & 0xFF
+            # clear the stream in preparation for the next frame
+            rawCapture.truncate(0)
+            # if the `q` key was pressed, break from the loop
+            if key == ord("q"):
+                break
+
+    def __get_rPI_cam_input(self):
+        from picamera.array import PiRGBArray
+        from picamera import PiCamera
+        # initialize the camera and grab a reference to the raw camera capture
+        camera = PiCamera()
+        camera.resolution = (640, 480)
+        camera.framerate = 32
+        rawCapture = PiRGBArray(camera, size=(640, 480))
+        return camera, rawCapture
+
+    def __predict_emotion_webcam(self):
+        video_captor = cv2.VideoCapture(0)
+        video_captor.set(cv2.CAP_PROP_FRAME_WIDTH, 1680)
+        video_captor.set(cv2.CAP_PROP_FRAME_HEIGHT, 1220)
+        timer = 19
+        while True:
+            timer = timer + 1
+            ret, frame = video_captor.read()
+            if timer % self.__prediction_conf['frame_per_second'] == 0:
+                self.__predict(frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    def get_data_from_camera(self, frame):
+        """
+        This function gets camera frame and returns normalized frame with detected faces
+        :param frame
+        :return: frame with detected face
+        """
+        if not self.__is_video_input():
+            image = frame
+            face_image = self.__face_detector.get_frame(image)
+            return face_image
+        else:
+            raise Exception("check input type and model format in configuration")
+
+    def __predict(self, frame):
+        if self.__prediction_conf['model_format'] == 'h5':
+            self.__h5_model_caller(frame)
+        elif self.__prediction_conf['model_format'] == 'IR':
+            self.__optimized_model_caller(frame)
+        else:
+            raise ValueError("model format is not correct")
+
+    def __h5_model_caller(self, frame):
+        if self.__is_video_input():
+            self.__h5_model_executer(frame, frame)
+        else:
+            face = self.__face_detector.get_frame(frame)
+            if face is not None:
+                normalized_face = self.__normalizer.get_frame(face)
+                self.__h5_model_executer(frame, normalized_face)
+
+    def __h5_model_executer(self, frame, normalized_face):
+        normalized_face_image = self.__image_reshape(normalized_face)
+        result = self.model.predict(normalized_face_image)
+        self.display_result(result, frame)
+
+    def __image_reshape(self, image):
+        return image.reshape(1, self.__prediction_conf['model_input_shape'][1]['height'],
+                             self.__prediction_conf['model_input_shape'][2]['width'],
+                             self.__prediction_conf['model_input_shape'][0]['channels'])
+
+    def __optimized_model_caller(self, frame):
+        if self.__is_video_input():
+            self.__optimized_model_executer(frame, frame)
+        else:
+            face = self.get_data_from_camera(frame)
+            if face is not None:
+                normalized_face = self.__normalizer.get_frame(face)
+                self.__optimized_model_executer(frame, normalized_face)
+            cv2.imshow("Frame", frame)
+
+    def __optimized_model_executer(self, frame, normalized_face):
+        prepared_face_image = self.__prepare_image(normalized_face,
+                                                   self.model,
+                                                   self.model_blob)
+        ir_result = self.model.infer(inputs={self.model_blob: prepared_face_image})
+        self.display_result(ir_result, frame)
+
     def __prepare_image(self, image, net, input_blob):
         n, c, h, w = net.input_info[input_blob].input_data.shape
         images = np.ndarray(shape=(n, c, h, w))
@@ -228,18 +247,27 @@ class Prediction:
             images[i] = image
         return images
 
-    def __predict_emotion_webcam(self):
-        video_captor = cv2.VideoCapture(0)
-        video_captor.set(cv2.CAP_PROP_FRAME_WIDTH, 1680)
-        video_captor.set(cv2.CAP_PROP_FRAME_HEIGHT, 1220)
-        timer = 19
-        while True:
-            timer = timer + 1
-            ret, frame = video_captor.read()
-            if timer % self.__prediction_conf['frame_per_second'] == 0:
-                self.__optimized_model_execution(frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+    def display_result(self, result, image):
+        """
+        This function shows histogram for the rPI camera input and/or provide a command line/text file representation
+        of the output data
+        :param result: result from prediction
+        :param image: captured image
+        """
+        if not self.__is_video_input():
+            if self.__prediction_conf['model_format'] == 'h5':
+                self.__gui_output.draw_histogram(result[0], image)
+                self.__logger.logs(result[0])
+            if self.__prediction_conf['model_format'] == 'IR':
+                for key, probes in result.items():
+                    ir_result = probes[0]
+                    self.__gui_output.draw_histogram(ir_result, image)
+                    self.__logger.logs(ir_result)
+        elif self.__is_video_input():
+            if self.__prediction_conf['model_format'] == 'h5':
+                self.__logger.logs(result)
+            if self.__prediction_conf['model_format'] == 'IR':
+                self.__logger.logs(list(result.values())[0])
 
 
 if __name__ == '__main__':
